8a670f586cff7974a08a27326f1d9b52eb4d121f
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-01-24 23:40:23 +0000

    nothing special, just to work on it in other laptop

---
+++ js.js
@@ -0,0 +1,22 @@
+
+// let divv = document.querySelector(".test")
+// // const div = document.createElement("div")
+// // function toggle(){
+// //     divv.classList.toggle("style1")
+// //     console.log(1)
+// // }
+// divv.addEventListener('click', function (e) {
+//     console.log(e.target);
+//   });
+
+// console.log(divv)
+
+// buttons is a node list. It looks and acts much like an array.
+let divv = document.querySelector(".test")
+
+// we use the .forEach method to iterate through each button
+
+const div = document.querySelectorAll("div")
+
+console.log(divv)
+

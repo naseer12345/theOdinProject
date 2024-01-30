4af2d50c3f61ba0ee25fddd3df07900d01faa106
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-01-30 21:33:03 +0000

    added the function to print out the content in the dom, it was kinda challengy tbh

--- intermediatHTMl-css-JS/library/index.js
+++ intermediatHTMl-css-JS/library/index.js
@@ -1,8 +1,27 @@
-function Book(bookName,author, number){
+const displayBooks = document.querySelector(".displayBooks")
+
+
+function Book(bookName,author, readornot ,pages){
     
     this.bookName = bookName;
     this.author = author;
-    this.number = number;
+    this.pages = pages;
+    this.readornot = readornot;
+    this.show = function(){
+        const div = document.createElement("div")
+        const bnP = document.createElement("p")
+        const aut = document.createElement("p")
+        const num = document.createElement("p")
+        const readOrnot = document.createElement("p")
+        bnP.textContent = this.bookName;
+        aut.textContent= this.author;
+        num.textContent = this.pages;
+        readOrnot.textContent = this.readornot;
+        div.append(bnP,aut,readOrnot,num)
+        displayBooks.appendChild(div)
+        console.log(div)
+
+    }
 
   
 
@@ -11,17 +30,15 @@ let arr = []
 
 function getvalue(event) {
     event.preventDefault(); // Prevent the default form submission behavior
-    const input = document.getElementById("bookName").value;
-    const inputt = document.getElementById("checkbox").checked;
-    const number = document.getElementById("number").value;
+    const bookname = document.getElementById("bookName").value;
+    const checkbox = document.getElementById("checkbox").checked;
+    const numberofpages = document.getElementById("number").value;
+    const author = document.getElementById("author").value;
 
-    const book = new Book(input, inputt, number)
-    console.log(book)
+    const book = new Book(bookname, author, checkbox, numberofpages)
     
-    Object.keys(book).forEach(key => {
-        console.log(book[key]);
-    });
 
+    book.show()
 
     
     
@@ -31,3 +48,33 @@ function getvalue(event) {
 }
 
 
+function printBooks(item){
+    }
+
+
+
+    const book1 = new Book(11,22, true)
+    arr.push(book1)
+    const book2= new Book(22,44,true)
+    arr.push(book2)
+    arr.map( item => printBooks(item))
+    
+
+    console.log(arr)
+
+
+
+
+
+
+//     Object.keys(item).map(key => {
+//         const div = document.createElement("div")
+//         const p = document.createElement("p")
+//         p.textContent = item[key]
+        
+//         displayBooks.appendChild(p)
+//         console.log(item[key])
+
+
+
+// })

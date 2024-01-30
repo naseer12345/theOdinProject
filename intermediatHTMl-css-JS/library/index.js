3b07e5c095da29cde9ddf34823b5236f084b843e
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-01-30 21:47:11 +0000

    changing laptop

--- intermediatHTMl-css-JS/library/index.js
+++ intermediatHTMl-css-JS/library/index.js
@@ -13,6 +13,10 @@ function Book(bookName,author, readornot ,pages){
         const aut = document.createElement("p")
         const num = document.createElement("p")
         const readOrnot = document.createElement("p")
+        const deletebtn = document.createElement("button")
+        deletebtn.addEventListener("onclick" , function deleteItemInArr(){
+            console.log(arr)
+        })
         bnP.textContent = this.bookName;
         aut.textContent= this.author;
         num.textContent = this.pages;
@@ -20,8 +24,10 @@ function Book(bookName,author, readornot ,pages){
         div.append(bnP,aut,readOrnot,num)
         displayBooks.appendChild(div)
         console.log(div)
-
+        arr.push(this)
+    
     }
+    this.remove()
 
   
 
@@ -29,52 +35,28 @@ function Book(bookName,author, readornot ,pages){
 let arr = []
 
 function getvalue(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
     const bookname = document.getElementById("bookName").value;
     const checkbox = document.getElementById("checkbox").checked;
     const numberofpages = document.getElementById("number").value;
     const author = document.getElementById("author").value;
 
     const book = new Book(bookname, author, checkbox, numberofpages)
-    
-
     book.show()
-
-    
-    
     document.getElementById("my-form").reset();
 
 
 }
 
 
-function printBooks(item){
-    }
 
-
-
-    const book1 = new Book(11,22, true)
-    arr.push(book1)
-    const book2= new Book(22,44,true)
-    arr.push(book2)
-    arr.map( item => printBooks(item))
+    // const book1 = new Book(11,22, true)
+    // arr.push(book1)
+    // const book2= new Book(22,44,true)
+    // arr.push(book2)
+    // arr.map( item => printBooks(item))
     
 
-    console.log(arr)
+    // console.log(arr)
 
 
-
-
-
-
-//     Object.keys(item).map(key => {
-//         const div = document.createElement("div")
-//         const p = document.createElement("p")
-//         p.textContent = item[key]
-        
-//         displayBooks.appendChild(p)
-//         console.log(item[key])
-
-
-
-// })

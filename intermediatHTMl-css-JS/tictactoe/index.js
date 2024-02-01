6e7bd9bfc5461a0230a3b13113db95c4df76b2b3
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-02-01 00:43:03 +0000

    i think i need some sleep, i will do tommorow while going to college

---
+++ intermediatHTMl-css-JS/tictactoe/index.js
@@ -0,0 +1,21 @@
+
+
+
+// check if 3x are equal and if 3o are equal
+// 1 for x and 0 for o  
+
+let arr = [1,1,1,0,0,1,1,1,1]
+function test (){
+let i = 0;
+if (arr[i] !== arr[i+1] || arr[i] !== arr[i+2] || arr[i+1] !== arr[i+2]) {
+    i 
+} else {
+    return (){
+        i+3
+    }
+    console.log("its equal");
+}();
+}
+
+
+test()

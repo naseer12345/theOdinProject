adb0a7e6f731a13f331b521113f7458bb70da393
naseer12345 <naseer.khan00128@gmail.com>
2024-02-01 12:33:24 +0000

    successfully made the solution checking logic

--- intermediatHTMl-css-JS/tictactoe/index.js
+++ intermediatHTMl-css-JS/tictactoe/index.js
@@ -4,18 +4,72 @@
 // check if 3x are equal and if 3o are equal
 // 1 for x and 0 for o  
 
-let arr = [1,1,1,0,0,1,1,1,1]
-function test (){
-let i = 0;
-if (arr[i] !== arr[i+1] || arr[i] !== arr[i+2] || arr[i+1] !== arr[i+2]) {
-    i 
-} else {
-    return (){
-        i+3
+
+
+
+
+// let arr = [0,1,1,1,0,1,1,0,1]
+// let i = 0;
+// function validatingFromleftToRight (){
+// if (arr[i] !== arr[i+1] ||  arr[i+1] !== arr[i+2]) {
+    
+//     console.log(i)
+//     console.log("its wrong")
+    
+//     if(i<=5){
+//     i = i+3;
+    
+//     validatingFromleftToRight()
+       
+//     }
+// } else{
+//     console.log("its right")
+    
+// };
+
+// }
+
+// validatingFromleftToRight()
+
+
+// let i = 0
+// function tst(){
+
+//     if(arr[i] !== arr[i+3] || arr[i+3] !== arr[i+6]){
+//     console.log(i)
+//     console.log("its wrong")
+    
+//     if(i<=1){
+//     i = i+1;
+    
+//     tst()
+       
+//     }
+// } else{
+//     console.log("its right")
+    
+// };
+
+// }
+// tst()
+
+let arr = [
+    0,0,0,
+    1,0,0,
+    1,0,0   
+]
+let i = 0
+function anothertst(){
+    if(arr[i] !== arr[i+4] || arr[i+4] !== arr[i+8]){
+        console.log("its wrong")
+    }else{
+        console.log("its right")
     }
-    console.log("its equal");
-}();
 }
 
+anothertst();
+    
 
-test()
+
+
+

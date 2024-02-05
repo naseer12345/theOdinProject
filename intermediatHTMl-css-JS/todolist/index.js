8e7e95b5c7ff89e528d348c13bd8c6420e525e8d
naseer12345 <naseer.khan00128@gmail.com>
2024-02-05 16:54:53 +0000

    just setting up the todo list project

---
+++ intermediatHTMl-css-JS/todolist/index.js
@@ -0,0 +1,16 @@
+// list of projects, every project will have different todos
+//         there should be a default project, you can either make one not deleteable or 
+//         you can make a function if there is no projects than 1 then we will be able to delete that.
+
+// what you can do is to have different arrays for different projects. by clicking the project we will map
+// inside the arrays or object and printout every data from it to the home screan
+
+
+// what we need now? we need a div on the left to show the current projects, also a button to make new projects
+// a home screen will be on the left, by clicking, it should display the project todos and then we will be able
+// to add new todos to that project as well.
+
+
+// we need a class for creating projects or to do list, i think the todo should be the main class
+// the project will be the child class. 
+// we need a static button to create for each project and todos

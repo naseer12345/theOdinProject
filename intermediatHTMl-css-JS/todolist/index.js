36362cc0b47ca41d8eee4f91c861227ff15ed227
naseer12345 <naseer.khan00128@gmail.com>
2024-02-07 15:36:08 +0000

    alsmost done, just need to fix a fucntion, thats it

--- intermediatHTMl-css-JS/todolist/index.js
+++ intermediatHTMl-css-JS/todolist/index.js
@@ -1,78 +1,48 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const inputTitle = document.querySelector('#title')
-const inputDescription = document.querySelector('#description')
-const inputDueDate = document.querySelector('#dueDate')
-
-
-
-
-
-
-
-
-// main objictive it to put making array for each of the createtasks in their own object
-// so when clicking the obj, we can call its array and print all of the insde divs
-// no matter how many projects we have, the contructor wwill make new arr for every obj
-
-
-// i think i need to have new array for every project, by creating a project i need to make new array
-// by clicking on any objects div i will then call that array, map through it and print what every its in it to the div, fuckkkkkk.
+// everything is working fine i guess, now the objective is how we wwill add new todos for each project. 
+// in the addtodobutton, i need to reffer to the projects obj so it will add the data into that object.
 
 let projectDiv = document.querySelector(".projects")
 let todosDiv = document.querySelector(".todos") 
 let addTodoBtn = document.querySelector(".addTodo")
+let listOfProjects = []
 let addProjectsBtn = document.querySelector(".addProjects")
 
-// let listOfProjects = []
-let listOfTodos = []
 
 
+
+// this is for getting the input for the todos and then adding to the obj array. 
+// we need to make a function, that will call a obj and add the data into its array.
+const inputTitle = document.querySelector('#title')
+const inputDescription = document.querySelector('#description')
+const inputDueDate = document.querySelector('#dueDate')
 addTodoBtn.addEventListener("click", ()=>{
-    console.log("add todoss bro ")
-    console.log(inputDescription.value)
+    
+    khan.addNewTodo(inputTitle.value,inputDescription.value,inputDueDate.value)
+    inputTitle.value = ''
+    inputDescription.value = ''
+    inputDueDate.value = ''
     
 })
-addProjectsBtn.addEventListener("click" , ()=>{
- const project = new CreateProjects("test", "description test" , '3 days')
- listOfProjects.push(project)
- console.log(listOfProjects)
-})
-
-// this function is for deleting elements inside the arrays of both i thing
-// function testing (objToDelete){
-//     listOfTodos =  listOfTodos.filter(something => something !==  objToDelete)
-
-// }
 
 
-function deleteTodoDiv(divToDelete){
-    console.log(listOfTodos)
-        listOfTodos =  listOfTodos.filter(something => something !==  divToDelete)
-        console.log(divToDelete)
-        divToDelete.remove()
-        console.log(listOfTodos)
 
-    
-   
+function addProjects(title){
+    const h = document.createElement("h1")
+    const button = document.createElement("button")
+    const div = document.createElement("div")
+    button.textContent = "Delete this project"
+    h.textContent = title;
+    button.addEventListener("click", ()=>{deleteProject(div)})
+    div.append(h,button)
+    projectDiv.appendChild(div)
+    listOfProjects.push(div)
+
 }
 
-
 // we will be calling this when we click on addTodo btn, its job is to just create the task. DONT SCANN, DO NOTHING ELSE
-function addTodos (title,description,date){
+function addTodos (title,description,date, obj){
+    console.log(obj.array)
     const taskDiv = document.createElement('div')
 
     const h = document.createElement("h1")
@@ -83,33 +53,50 @@ function addTodos (title,description,date){
     h.textContent = title;
     p.textContent = description;
     dueDate.textContent = date
-    button.addEventListener("click", () => deleteTodoDiv(taskDiv))
+    button.addEventListener("click", () => deleteTodoDiv(taskDiv , obj))
     taskDiv.append(h,p,dueDate,button)
     todosDiv.appendChild(taskDiv)
-    listOfTodos.push(taskDiv)
+    obj.array.push(taskDiv)
 
-    return
+    return taskDiv
    
    
 }
-function addProjects(title, description, date){
-    const h = document.createElement("h1")
-    const p = document.createElement("p")
-    const dueDate = document.createElement("p")
-    const button = document.createElement("button")
-    button.textContent = "Delete this todo"
-    h.textContent = title;
-    p.textContent = description;
-    dueDate.textContent = date
-    button.addEventListener("click", deleteDiv)
-    todosDiv.append(h,p,dueDate,button)
 
+
+   // this function is for deleting elements inside the arrays of both i thing
+   function deleteProject(div){
+    listOfProjects =  listOfProjects.filter(item => item !==  div)
+        console.log(div)
+         div.remove()
+         console.log(listOfProjects)
+ 
+    
+    }
+    
+
+function deleteTodoDiv(divToDelete, obj){
+    // console.log(listOfTodos)
+        obj.array = obj.array.filter(something => something !==  divToDelete)
+        console.log(divToDelete)
+        divToDelete.remove()
+        console.log(obj.array)
+
+    
+   
 }
 
 
 
+// objs should have array that will contain all the taks divs. so when we click on every projects div we will scann the objects array t print the divs 
 
-
+function CreateProjects(title){
+    this.title = title 
+    this.todosForThisObj = new CreateTasks()
+    this.addProject = function(){
+        addProjects(title)
+    }
+}
 
 
 
@@ -117,39 +104,49 @@ function CreateTasks(title, description,duedate ){
     this.title = title;
     this.description = description;
     this.duedate = duedate;
+    this.array = []
+   
     this.addTodo = function(){
-        addTodos(title, description,duedate)
+        
+        addTodos(title, description,duedate ,this)    }
+
+    // this deletes the div inside the objects array, worked when i had only one item in the array
+    this.deleteTodo = function(){
+        this.array =  this.array.filter(something => something !==  taskDiv)
+    }
+    // now this adds new div to the objs array, which is good.
+    this.addNewTodo = function(newTitle, newDescription, newDate){
+        addTodos(newTitle,newDescription,newDate , this)
+
+    }
+    this.printObjsDivs = function(){
+        this.array.map(item =>{
+            console.log(item)
+        })
     }
 
 }
 
-// objs should have array that will contain all the taks divs. so when we click on every projects div we will scann the objects array t print the divs 
-
-// this obj inhereting bug
-function CreateProjects(title, projectdescription, dueDate){
-    const {description, duedate} = new CreateTasks(title, this.projectdescription , this.dueDate)
-    this.title = title 
-
-    this.addProject = function(){
-        addProjects(title, description,duedate)
-    }
-}
 
 
-const khan = new CreateTasks("blah blah","nothing to add", "4")
-const khan2 = new CreateTasks("a project name" , "project description" , "34")
-const khan3 = new CreateTasks("a project name 3" , "project description 3" , "3")
-
-khan.addTodo()
-khan2.addTodo()
-khan3.addTodo()
-
-// console.log(listOfTodos[0].outerText)
 
 
-// listOfTodos.map(obj=>{
+
+
+
+addProjectsBtn.addEventListener("click" , ()=>{
+    const inputProjectDescription = document.querySelector("#inputPorjectDescription")
+    const project = new CreateProjects(inputProjectDescription.value)
+    project.addProject()
+    inputProjectDescription.value= ''
+
     
-//     console.log(obj.title, obj.description , obj.duedate)
-// })
+   })
+   
 
+   
+   
+  
+   
 
+ 
